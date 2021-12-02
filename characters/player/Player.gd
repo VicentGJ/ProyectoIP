7fd775b837faf2677518e4bc8aa7d2e9ffe9634d
VicentGJ <vsgarofalojerez@gmail.com>
2021-12-02 18:51:33 -0500

    godot project created

---
+++ characters/player/Player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var speed = 200  # speed in pixels/sec
+
+var velocity = Vector2.ZERO
+
+func get_input():
+	velocity = Vector2.ZERO
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x += 1
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= 1
+	if Input.is_key_pressed(KEY_DOWN):
+		velocity.y += 1
+	if Input.is_key_pressed(KEY_UP):
+		velocity.y -= 1
+	# Make sure diagonal movement isn't faster
+
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
