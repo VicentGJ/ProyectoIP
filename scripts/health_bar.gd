00d90939e3f21e51901a5ace45cdb83a9f3cb3ff
Carlos-err406 <carlosvilaseca406@gmail.com>
2022-01-08 15:10:00 -0400

    health bar scene
    new project folder structure, scripts in scripts folder, sprites in sprites folder etc

---
+++ scripts/health_bar.gd
@@ -0,0 +1,15 @@
+extends Control
+
+onready var health_over = $health_over
+onready var health_under = $health_under
+onready var tween = $Tween
+
+func damaged(damage):
+	tween.interpolate_property(health_under, "value", health_over.value,health_over.value - damage, 0.4,Tween.TRANS_LINEAR,Tween.EASE_IN,0.1)
+	tween.start()
+	health_over.value -= damage
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("attack"):
+		damaged(10)
+	
