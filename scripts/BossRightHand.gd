f30ef68790162104b89474c3948fb2095d2a5b5a
Carlos-err406 <carlosvilaseca406@gmail.com>
2022-01-16 22:24:41 -0400

    boss

--- scripts/BossRightHand.gd
+++ scripts/BossRightHand.gd
@@ -1,11 +1,18 @@
 extends KinematicBody2D
 var movementLimit
-
+var handLife = 21 #7 full combos of the player
+onready var handMove = $StandartMove
+onready var horizontalAttack = $HorizontalAttack
 func _ready():
 	pass
+
 #func damage
-#func damaged
 
+func damaged():
+	handLife -= 1
 
+func horizontalAttack():
+	horizontalAttack.interpolate_property(self,"position:x",position.x,position.x + 200, 1,Tween.TRANS_BACK,Tween.EASE_IN)
+	
 func _process(delta):
 	pass
