3d199a2403895c5a26f05fd6dbee2fef9f7ad830
Carlos Daniel Vilaseca <81443707+Carlos-err406@users.noreply.github.com>
2022-01-21 16:50:33 -0500

    platform fixed
    los enemigos tienen que estar en la capa 1 y 3

--- scripts/props/coin.gd
+++ scripts/props/coin.gd
@@ -4,12 +4,12 @@ onready var coin = $Sprite
 onready var toQueueFree = $queueFreeTimer
 onready var leftRay = $left
 onready var rightRay = $right
-
+onready var player = get_parent().get_node("player")
 export var coinValue = 10
 
 
 func _physics_process(delta):
-	if leftRay.is_colliding() or rightRay.is_colliding():
+	if leftRay.get_collider() == player or leftRay.get_collider() == player:
 		tween.interpolate_property(self, "position", position,Vector2(position.x , position.y - 30),0.5,Tween.TRANS_LINEAR,Tween.EASE_IN) 
 		tween.start()
 		tween.interpolate_property(coin, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 0.5,Tween.TRANS_BOUNCE)
--- scripts/props/platform.gd
+++ scripts/props/platform.gd
@@ -2,8 +2,10 @@ extends StaticBody2D
 
 func _process(delta):
 	if Input.is_action_pressed("ui_down") and Input.is_action_pressed("jump"):
-		$CollisionShape2D.set_disabled(true)
+		set_collision_layer(4)
+		set_collision_mask(4)
 		$restoreCollision.start()
 	
 func _on_restoreCollision_timeout():
-	$CollisionShape2D.set_disabled(false)
+	set_collision_layer(3)
+	set_collision_mask(3)
