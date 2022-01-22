37aaf6a409cb31d03566047281e0f20584dd4ee5
Carlos Daniel Vilaseca <81443707+Carlos-err406@users.noreply.github.com>
2022-01-22 12:59:31 -0500

    keys sprites and particles

--- scripts/props/keyRare.gd
+++ scripts/props/keyRare.gd
@@ -6,7 +6,7 @@ onready var toQueueFree = $queueFreeTimer
 onready var leftRay = $left
 onready var rightRay = $right
 onready var player = get_parent().get_node("player")
-
+onready var particles = $Particles2D
 export var price = 0
 var collected = false
 
@@ -15,6 +15,7 @@ signal collected()
 func _physics_process(delta):
 	if not collected and (leftRay.get_collider() == player or leftRay.get_collider() == player):
 		if Input.is_action_just_pressed("interact") and player.money >= price:
+			particles.set_emitting(false)
 			tween.interpolate_property(self, "position", position,Vector2(position.x , position.y - 30),0.5,Tween.TRANS_LINEAR,Tween.EASE_IN) 
 			tween.start()
 			tween.interpolate_property(key, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 0.5,Tween.TRANS_BOUNCE)
