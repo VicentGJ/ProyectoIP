11094990991b9eafe260c8ce7ca8dc384e3b1916
Carlos Daniel Vilaseca <81443707+Carlos-err406@users.noreply.github.com>
2022-01-22 10:20:39 -0500

    Update door.gd

--- scripts/props/door.gd
+++ scripts/props/door.gd
@@ -7,8 +7,17 @@ onready var particles = $Particles2D
 onready var door = $Sprite
 onready var player = get_parent().get_node("player")
 
+export var isDoorRare = false
+
+func _ready():
+	if isDoorRare:
+		$Sprite.frame = 2
+	else:
+		$Sprite.frame = 0
+
 func open():
-	pass
+	if isDoorRare:
+		$AnimationPlayer.play("open")
 
 func _process(delta):
 	if Input.is_action_just_pressed("interact") and (leftRay.get_collider() == player \
