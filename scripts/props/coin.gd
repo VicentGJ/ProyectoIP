5d944e4f8fc74641197e9fd4bed3f4460514d3e6
Carlos Daniel Vilaseca <81443707+Carlos-err406@users.noreply.github.com>
2022-01-22 14:11:57 -0500

    coin completed
    process material
    coins
    coins prices
    coins particles

--- scripts/props/coin.gd
+++ scripts/props/coin.gd
@@ -6,14 +6,35 @@ onready var leftRay = $left
 onready var rightRay = $right
 onready var player = get_parent().get_node("player")
 export var coinValue = 10
+onready var particles = $Particles2D
+onready var materialGold = preload("res://effects/coinGoldParticles.tres")
+onready var materialSapphire = preload("res://effects/coinSapphireParticles.tres")
+onready var materialRuppies = preload("res://effects/coinRupiesParticles.tres")
+
+export var coinType = [0, 1, 2]
+var random
 
 var collected = false
-
+func _ready():
+	randomize()
+	random = randi() % 11
+	if random < 6:
+		coin.set_modulate(Color.gold)
+		coinValue = 10
+		particles.set_process_material(materialGold)
+	elif random < 9:
+		coin.set_modulate(Color.royalblue)
+		coinValue = 20
+		particles.set_process_material(materialSapphire)
+	else:
+		coin.set_modulate(Color.crimson)
+		coinValue = 30
+		particles.set_process_material(materialRuppies)
 func _physics_process(delta):
 	if not collected and (leftRay.get_collider() == player or leftRay.get_collider() == player):
 		tween.interpolate_property(self, "position", position,Vector2(position.x , position.y - 30),0.5,Tween.TRANS_LINEAR,Tween.EASE_IN) 
 		tween.start()
-		tween.interpolate_property(coin, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 0.5,Tween.TRANS_BOUNCE)
+		tween.interpolate_property(coin, "modulate", coin.get_modulate(), Color(1, 1, 1, 0), 0.5,Tween.TRANS_LINEAR)
 		tween.start()
 		collected = true
 		toQueueFree.start()
