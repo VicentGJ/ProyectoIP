ee0e2e191fd53954964a0c1d9a8225673172339c
Carlos Daniel Vilaseca <81443707+Carlos-err406@users.noreply.github.com>
2022-01-23 09:12:45 -0500

    sdad

--- scripts/enemies/trap.gd
+++ scripts/enemies/trap.gd
@@ -1,14 +1,14 @@
 extends Area2D
 
-onready var player = get_parent().get_node("player")
+onready var player = get_parent().get_parent().get_node("player")
 
 var canTrigger = false
 
-export var trapDamage = -15
+export var trapDamage = -50
 
 
 func _on_trap_body_entered(body):
-	if canTrigger:
+	if canTrigger and body == player:
 		player.changeStats(0, trapDamage)
 		$reactivate.start()
 		canTrigger = false
