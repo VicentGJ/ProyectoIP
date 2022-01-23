cf3eceab6af5179988e269bf184b857c6636a7cc
VicentGJ <vsgarofalojerez@gmail.com>
2022-01-22 20:58:05 -0500

    verj label

--- scripts/props/verj.gd
+++ scripts/props/verj.gd
@@ -4,12 +4,17 @@ onready var player = get_parent().get_node("player")
 onready var leftRay = $left
 onready var rightRay = $right
 onready var particles = $Particles2D
+onready var tween = $Tween
+onready var label = $Label
 var collected = false
 
-signal varjCollected()
 
 func _process(delta):
 	if not collected and (leftRay.get_collider() == player or leftRay.get_collider() == player):
+		label.set_modulate(Color(1,1,1,1))
 		if Input.is_action_just_pressed("interact"):
+			label.set_modulate(Color(1,1,1,0))
 			particles.set_emitting(false)
-			emit_signal("varjCollected")
+			player.changeDimension()
+			collected = true
+
